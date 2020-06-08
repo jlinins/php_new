06af866b4faf1c31bfa8714a5cf9f395b33e6f8f
Jānis Liniņš <mydarkestday@outlook.com>
2020-06-08 12:25:10 +0300

    renamed folder

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+<h1>Simple HTML</h1>
+<?php
+echo "<div class='myclass'> Anything goes here </div> "
+?>
+<h2>Heading2 </h2>
+<?php
+for ($i =0; $i<5; $i++){
+    echo "<p>Paragraph no $i</p>";
+}
+?>
+<footer>Not done by html</footer>
+
+    
+</body>
+</html>
