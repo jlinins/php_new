83f0f065e014f94a4e855d883711c595e523af4c
Jānis Liniņš <mydarkestday@outlook.com>
2020-06-08 13:07:42 +0300

    ..

--- public/syntax.php
+++ public/syntax.php
@@ -1,2 +1,15 @@
 <?php
 echo "Syntax! <hr>";
+$txt ="Hello World";
+$x= 5;
+$y=10.5;
+$result= $x+$y; 
+// var dumb for debuging 
+var_dump($txt);
+$a= 2;
+$b=10;
+$name= "Jānis ";
+$lastname="Puncis";
+
+$mix= $name. "is" . $lastname;
+
